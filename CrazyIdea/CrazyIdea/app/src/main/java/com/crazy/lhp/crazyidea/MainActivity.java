abaf977eba92b6659559b4d9c9271304b9e27ebe
lianghp <lianghp@meixing.com>
2016-05-18 11:41:36 +0800

    同步代码

--- CrazyIdea/CrazyIdea/app/src/main/java/com/crazy/lhp/crazyidea/MainActivity.java
+++ CrazyIdea/CrazyIdea/app/src/main/java/com/crazy/lhp/crazyidea/MainActivity.java
@@ -1,31 +1,37 @@
 package com.crazy.lhp.crazyidea;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.crazy.lhp.crazyidea.custom.CustomActivity;
+
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private static final String TAG = "lianghp MainActivity";
 
+    private Button btn_tl_test_1;//自定义控件，小球随着手指在屏幕上移动而移动
+
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG,"MainActivity onStart()");
+        Log.d(TAG, "MainActivity onStart()");
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "MainActivity onCreate()");
         setContentView(R.layout.activity_main);
+        initwidget();
     }
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG,"MainActivity onResume()");
+        Log.d(TAG, "MainActivity onResume()");
     }
     @Override
     protected void onPause() {
@@ -45,14 +51,20 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG,"MainActivity onDestory()");
+        Log.d(TAG, "MainActivity onDestory()");
+    }
+
+    public void initwidget(){
+        btn_tl_test_1 = (Button)findViewById(R.id.btn_tl_test_1);
     }
 
     @Override
     public void onClick(View view) {
         int Id = view.getId();
         switch (Id){
-            case 0:
+            case R.id.btn_tl_test_1:
+                Intent intent = new Intent(this, CustomActivity.class);
+                startActivity(intent);
                 break;
             default:
                 break;
---
+++ CrazyIdea/CrazyIdea/app/src/main/java/com/crazy/lhp/crazyidea/custom/CustomActivity.java
@@ -0,0 +1,22 @@
+package com.crazy.lhp.crazyidea.custom;/**
+ * Author：Lianghp on 2016/5/18 08:37
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.crazy.lhp.crazyidea.R;
+
+/**
+ * Date: 2016-05-18
+ * Time: 08:37
+ * Note:  There is nothing about it!
+ * FIXME
+ */
+public class CustomActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom);
+    }
+}
